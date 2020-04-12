6c8dfbd443c88a7ecef81d0c00879e93eff52ff6
dengyonghao109 <dengyonghao109@pingan.com.cn>
2020-04-12 22:54:52 +0800

    add BaseProject

---
+++ BaseProject/BaseProject/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BaseProject
+//
+//  Created by dengyonghao on 2019/12/31.
+//  Copyright © 2019 DengYonghao. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
